b40f3fae146ee7ca6855bdb96b0d564145061a4b
Ravneet Kaur <ravneet.kaur@anotechsolutions.com>
2024-05-18 12:38:28 +0000

    Ravneet

---
+++ src/Components/Rav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rav() {
+  return (
+    <div>
+      <h1>Hi,Ravneet here!</h1>
+    </div>
+  )
+}
+
+export default Rav
---
+++ src/Components/Ravneet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ravneet() {
+  return (
+    <div>
+      <h1>Hi,Ravneet here!</h1>
+    </div>
+  )
+}
+
+export default Ravneet
