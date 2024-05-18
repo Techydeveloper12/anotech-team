33517a8db2dfad244466bf657ea8dd857a71145e
mondira06 <mondira.chanda@anotechsolutions.com>
2024-05-18 11:37:11 +0000

    mondira

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Sayandeep from './Components/1';
 import Func1 from './Components/2';
+import Mondira from './Components/3';
 function App() {
   return (
     <div className="App">
       <Sayandeep/>
       <Func1/>
+      <Mondira/>
     </div>
   );
 }
